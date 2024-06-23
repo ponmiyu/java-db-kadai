6f0d65b919ac2e1ccb4787b4c03bf5317687931f
ponmiyu <emuponzu821@icloud.com>
2024-06-23 16:16:01 +0900

    課題提出

---
+++ java_db_study/src/kadai_010/Scores_Chapter10.java
@@ -0,0 +1,66 @@
+package kadai_010;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Scores_Chapter10 {
+
+	public static void main(String[] args) {
+
+		Connection con = null;
+		Statement statement = null;
+
+		try {
+			//データベースに接続する
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost/challenge_java",
+					"root",
+					"samuraipon");
+			System.out.println("データベース接続成功：" + con);
+
+			//SQLクエリを準備する
+			statement = con.createStatement();
+			String sql1 = "UPDATE scores SET score_math = 95, score_english = 80 WHERE id = 5;";
+
+			//点数データを更新する
+			System.out.println("レコード更新を実行します");
+			int rowCnt = statement.executeUpdate(sql1);
+			System.out.println(rowCnt + "件のレコードが更新されました");
+
+			//点数順に並べる
+			String sql2 = "SELECT * FROM scores ORDER BY score_math DESC, score_english DESC;";
+			ResultSet result = statement.executeQuery(sql2);
+			System.out.println("数学・英語の点数が高い順に並べ替えました");
+
+			//並べ替え結果を表示する
+			while (result.next()) {
+				int id = result.getInt("id");
+				String name = result.getString("name");
+				int scoreMath = result.getInt("score_math");
+				int scoreEnglish = result.getInt("score_english");
+				System.out.println(
+						result.getRow() + "件目：生徒ID=" + id + "/氏名=" + name + "/数学=" + scoreMath + "/英語=" + scoreEnglish);
+			}
+		} catch (SQLException e) {
+			System.out.println("エラー発生：" + e.getMessage());
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException ignore) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ignore) {
+				}
+			}
+		}
+
+	}
+
+}
